48cfc5b0fd302c9c03bb5381b58d781884d58572
Saranya <saranya3606@gmail.com>
2022-11-14 21:50:52 +0530

    First Time

---
+++ src/test/java/org/omg/GitClass.java
@@ -0,0 +1,36 @@
+package org.omg;
+
+public class GitClass {
+     private void method1() {
+		System.out.println("apple");
+	}
+     
+     private void method2() {
+    	 System.out.println("orange");
+ 	}
+     
+     private void method3() {
+    	 System.out.println("mango");
+
+ 	}
+     
+     
+     
+     
+     
+     
+     
+     
+     
+     
+     
+     
+     
+     
+     
+     
+     
+     
+     
+     
+}
